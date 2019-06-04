cc97ad4ee8084aaab33c66249de0aea9a77edb16
juan <juan@LAPTOP-9GB8C7NU>
2019-06-03 21:25:21 -0500

    avances

--- src/proyecto2/representacion.java
+++ src/proyecto2/representacion.java
@@ -11,58 +11,162 @@ public class representacion {
 
 	public static grafo solution(String archivo) throws IOException {
 		String cadena;
-	      FileReader f = new FileReader(archivo);
-	      BufferedReader b = new BufferedReader(f);
-	      grafo graph = new grafo();
-	      cadena=b.readLine();
-	      while(cadena !=null) {
-	          String[]arreglo=cadena.split(" ");
-	          nodografo nuevo=new nodografo(arreglo[0]+"");
-	          nodoconexion nuevo2=new nodoconexion(arreglo[1]+"");
-	          if (graph.nodos.binarySearch(nuevo)==-1) {
-	        	  graph.añadirnodo(nuevo);
-	        	  nuevo.conexiones.insertAtEnd(nuevo2);
+		FileReader f = new FileReader(archivo);
+		BufferedReader b = new BufferedReader(f);
+		grafo graph = new grafo();
+		cadena=b.readLine();
+		while(cadena !=null) {
+			String[]arreglo=cadena.split(" ");
+			nodografo nuevo=new nodografo(arreglo[0]+"");
+			nodoconexion nuevo2=new nodoconexion(arreglo[1]+"");
+			if (graph.nodos.binarySearch(nuevo)==-1) {
+				graph.añadirnodo(nuevo);
+				nuevo.conexiones.insertAtEnd(nuevo2);
 			}else {
 				int posicion = graph.nodos.binarySearch(nuevo);
 				nodografo c=(nodografo)graph.nodos.get(posicion);
 				c.conexiones.insertAtBegin(nuevo2);
 			}
-	          
-	          
-	        
+
+
+
 			cadena=b.readLine();
-	       
-	      }
-	      b.close();
+
+		}
+		b.close();
+
+		return graph;
+	}
+
+	public static  int lista_mayor(grafo graph) {
+		nodografo tempmayor=(nodografo)graph.nodos.head;
+		int mayor=tempmayor.conexiones.length();
+		String etiqueta=tempmayor.etiqueta;
+		while (tempmayor!=null) {
+			if(tempmayor.conexiones.length()>mayor  ) {
+				mayor=tempmayor.conexiones.length();
+				etiqueta=tempmayor.etiqueta;
+			}
+			tempmayor=tempmayor.next;
+		}
+		return mayor;
+
+	}
+	public static int[] particion(grafo graph){
+		int[] arreglo=new int[5];
+		nodografo tempmayor=(nodografo)graph.nodos.head;
+		int contador1=0,contador2=0,contador3=0,contador4=0,contador5=0;
+
+		while (tempmayor!=null) {
+
+			if(tempmayor.conexiones.length()<=50) {
+				contador1++;
+			}else if(tempmayor.conexiones.length()>50&&tempmayor.conexiones.length()<=100) {
+				contador2++;
+			}else if(tempmayor.conexiones.length()>100&&tempmayor.conexiones.length()<=150) {
+				contador3++;
+			}else if(tempmayor.conexiones.length()>150&&tempmayor.conexiones.length()<=200) {
+				contador4++;
+			}else if(tempmayor.conexiones.length()>200) {
+				contador5++;
+			}
+
+			tempmayor=tempmayor.next;
+		}
+		arreglo[0]=contador1;
+		arreglo[1]=contador2;
+		arreglo[2]=contador3;
+		arreglo[3]=contador4;
+		arreglo[4]=contador5;
 	
-        return graph;
+
+		return arreglo;
+
+	}
+	public static int[] particion2(grafo graph){
+		int[] arreglo=new int[5];
+		nodografo tempmayor=(nodografo)graph.nodos.head;
+		int contador1=0,contador2=0,contador3=0,contador4=0,contador5=0;
+
+		while (tempmayor!=null) {
+
+			if(tempmayor.conexiones.length()<=10) {
+				contador1++;
+			}else if(tempmayor.conexiones.length()>10&&tempmayor.conexiones.length()<=20) {
+				contador2++;
+			}else if(tempmayor.conexiones.length()>20&&tempmayor.conexiones.length()<=30) {
+				contador3++;
+			}else if(tempmayor.conexiones.length()>30&&tempmayor.conexiones.length()<=40) {
+				contador4++;
+			}else if(tempmayor.conexiones.length()>40 &&tempmayor.conexiones.length()<=50 ) {
+				contador5++;
+			}
+
+			tempmayor=tempmayor.next;
+		}
+		arreglo[0]=contador1;
+		arreglo[1]=contador2;
+		arreglo[2]=contador3;
+		arreglo[3]=contador4;
+		arreglo[4]=contador5;
+
+		return arreglo;
+
 	}
 	
-	public static int lista_larga(grafo graph) {
-		nodografo tempmayor=(nodografo)graph.nodos.get(0);
-		int mayor=tempmayor.conexiones.length();
-		int j=0;
-		for (int i = 0; i < graph.nodos.length(); i++) {
-			if(i!=3564) {
+	public static int promedio(grafo graph) {
+		nodografo tempmayor=(nodografo)graph.nodos.head;
+		int acumulador=0;
+
+		while (tempmayor!=null) {
+			if(tempmayor.conexiones.length()<=50) {
 				
 			
-			nodografo temp=(nodografo)graph.nodos.get(i);
-			int mayor2=temp.conexiones.length();
-			if(mayor2>mayor) {
-				mayor=mayor2;
-				j=i;
+			acumulador=acumulador+tempmayor.conexiones.length();
 			}
+			tempmayor=tempmayor.next;
 		}
-		}
-		return j;
+		
+		
+		return acumulador/graph.nodos.length();
 
 	}
-public static void main(String[] args) throws IOException {
-        grafo graph = solution("C:\\Users\\julian\\Desktop\\1111");
-        graph.imprimir();
-      
-    
+	public static double desviacion(grafo graph) {
+		nodografo tempmayor=(nodografo)graph.nodos.head;
+		int acumulador=0;
+		int media=promedio(graph);
+		
+		while (tempmayor!=null) {
+			int valor=0;
+			if(tempmayor.conexiones.length()<=50) {
+			valor=(int)Math.pow(tempmayor.conexiones.length()-media, 2);
+			acumulador=acumulador+valor;
+			}
+			tempmayor=tempmayor.next;
+		}
+		double desviacion_estandar=Math.sqrt(acumulador/3171);
+		
+		return desviacion_estandar;
 
-}
+	}
+	public static double coeficiente(grafo graph) {
+		nodografo tempmayor=(nodografo)graph.nodos.head;
+		double acumulador=desviacion(graph);
+		int media=promedio(graph);
+		return  (acumulador/media)*100;
+		
+	}
+	public static void main(String[] args) throws IOException {
+		grafo graph = solution("C:\\Users\\57314\\Desktop\\grafo");
+		System.out.println(graph.nodos.length()+" longitud grafo");
+		//		System.out.println(lista_mayor(graph));
+//		int[]arreglo=particion(graph);
+//for (int i = 0; i < arreglo.length; i++) {
+//	System.out.println(arreglo[i]);
+//}
+		System.out.println(coeficiente(graph));
+		System.out.println(desviacion(graph));
+		//System.out.println(" promedio "+promedio(graph));
+	}
 }
 
